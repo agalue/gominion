fb5fdf6ec1cae707834f6bc3225cbcaca13156c3
Alejandro Galue <agalue@opennms.org>
2021-03-28 10:47:02 -0400

    Fix code generation with protoc.

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -2,9 +2,9 @@
 
 type protoc >/dev/null 2>&1 || { echo >&2 "protoc required but it's not installed; aborting."; exit 1; }
 
-protoc -I . ./proto/sink-message.proto --go_out=./sink
-protoc -I . ./proto/kafka-rpc.proto --go_out=./rpc
-protoc -I . ./proto/telemetry.proto --go_out=./telemetry
-protoc -I . ./proto/netflow.proto --go_out=./netflow
-protoc -I . ./proto/ipc.proto --go_out=plugins=grpc:./ipc
-protoc -I . ./proto/mdt_dialout.proto --go_out=plugins=grpc:./mdt_dialout
+protoc --proto_path=./proto --go_out=./ sink-message.proto
+protoc --proto_path=./proto --go_out=./ kafka-rpc.proto
+protoc --proto_path=./proto --go_out=./ telemetry.proto
+protoc --proto_path=./proto --go_out=./ netflow.proto
+protoc --proto_path=./proto --go_out=plugins=grpc:./ ipc.proto
+protoc --proto_path=./proto --go_out=plugins=grpc:./ mdt_dialout.proto
